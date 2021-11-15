3f1cf9493ea5b2faf6cdd36128c58de1b2e870fe
Kalyan Mekala <kalyan.mekala@tui.be>
2021-11-15 22:36:15 +0530

    change: changes to support APIGateway and lambda events as request sources

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"reflect"
 	"ruleEngineProject/handlers"
+	"strings"
 
 	"errors"
 	"github.com/aws/aws-lambda-go/events"
@@ -28,7 +31,7 @@ func main() {
 //	JourneyType            string `json:"journeyType"`
 //}
 
-func HandleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+/*func HandleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	//rb := name
 
 	//if req.Headers["Httpmethod"] == http.MethodPost{
@@ -43,6 +46,58 @@ func HandleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 		return events.APIGatewayProxyResponse{Body: "Method not ok", StatusCode: http.StatusBadRequest, IsBase64Encoded: false, Headers: nil}, err
 	}
 
+}*/
+
+func HandleRequest(input interface{}) (interface{}, error) {
+	log.Println("type:", reflect.TypeOf(input))
+
+	var req events.APIGatewayProxyRequest
+	var err error
+
+	inputRequest, err := json.Marshal(input)
+	if err != nil {
+		log.Println("unable to marshal input to json")
+	}
+	//log.Println(string(inputRequest))
+	inputRequestAsString := string(inputRequest)
+	containsHeader := strings.Contains(inputRequestAsString, "headers")
+	err = json.Unmarshal(inputRequest, &req)
+	if err != nil || !containsHeader {
+		log.Println("unable to unmarshal input from json to APIGatewayRequest type or contains header is ", containsHeader)
+		apiResponse, err := performRuleCheck(inputRequestAsString)
+		if err != nil {
+			return nil, err
+		} else {
+			return apiResponse, err
+		}
+	} else if containsHeader {
+		log.Println("request path", req.Path)
+		if req.HTTPMethod == http.MethodPost {
+			apiResponse, err := performRuleCheck(req.Body)
+			if err != nil {
+				return events.APIGatewayProxyResponse{Body: "Couldn't perform rule check", StatusCode: http.StatusOK, IsBase64Encoded: false, Headers: nil}, nil
+			} else {
+				return events.APIGatewayProxyResponse{Body: apiResponse, StatusCode: http.StatusOK, IsBase64Encoded: false, Headers: nil}, nil
+			}
+
+		} else {
+			err = errors.New("method not allowed NavyA" + req.Headers["Httpmethod"] + ":lambda ")
+		}
+
+	} else {
+		err = errors.New("nothing executed")
+	}
+
+	return events.APIGatewayProxyResponse{Body: "Nothing Executed", StatusCode: http.StatusOK, IsBase64Encoded: false, Headers: nil}, err
+
+}
+
+func performRuleCheck(request string) (string, error) {
+	l := log.New(os.Stdout, "Rules-API", log.LstdFlags)
+	rc := handlers.NewRuleChecker(l)
+	apiResponse := string(rc.GetRuleCheck(request))
+
+	return apiResponse, nil
 }
 
 //func invokeRuleEngineLambda(request string) (response []byte, err error) {
